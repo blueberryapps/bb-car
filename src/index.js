afc908c60e873dc698a8822147487ceef2f75905
Antonin <antonin.hackenberg@gmail.com>
2017-07-23 00:30:05 +0200

    Tiny refactor && clean console

--- src/index.js
+++ src/index.js
@@ -28,8 +28,6 @@ export default class AnimationExample extends Component {
     this.axisX = axisX;
     this.axisY = axisY;
 
-
-
     this.axisXDefault = axisX;
     this.axisYDefault = axisY;
 
@@ -53,13 +51,13 @@ export default class AnimationExample extends Component {
       onMoveShouldSetResponderCapture: () => true,
       onMoveShouldSetPanResponderCapture: () => true,
       onPanResponderGrant: (e) => {
-
-        this.sendData()
         this.state.panAnimation.setOffset({ x: this.axisX, y: this.axisY });
         this.state.panAnimation.setValue({ x: 0, y: 0 });
         Animated.spring(this.state.scaleAnimation, {
           toValue: 1.5,
         }).start();
+        // Start to send data to Car
+        this.sendData()
       },
       onPanResponderMove: Animated.event([
         null, { dx: this.state.panAnimation.x, dy: this.state.panAnimation.y },
@@ -69,16 +67,9 @@ export default class AnimationExample extends Component {
           toValue: 1,
         }).start();
 
-
-        this.interval && clearInterval(this.interval)
-
-        const { width, height } = Dimensions.get('window');
-        const edgeLength = 100;
-
-        const axisX = (width / 2) - (edgeLength / 2);
-        const axisY = (height / 2) - (edgeLength / 2);
-
+        // Clear coordinates
         this.state.panAnimation.setValue({ x: 0, y: 0 });
+        this.interval && clearInterval(this.interval)
       }
     });
 
@@ -90,13 +81,10 @@ export default class AnimationExample extends Component {
       .then((values) => {
         const [ isEnabled, devices ] = values
 
-        console.log(isEnabled, devices)
-
-        const [device] = devices.filter(item => {
+        const [device = {}] = devices.filter(item => {
           return item.name.startsWith('HC-05')
         })
-
-        console.log(device)
+        this.setState({ isEnabled, device })
 
         BluetoothSerial.connect(device.id)
           .then((res) => {
@@ -108,9 +96,6 @@ export default class AnimationExample extends Component {
             alert('Amigo, no funciona!!')
             this.setState({connected: false })
           })
-
-
-        this.setState({ isEnabled, device: device[0] })
       })
   };
 
@@ -120,23 +105,27 @@ export default class AnimationExample extends Component {
     clearInterval(this.interval)
   };
 
-
-
   calculatePower = ({x, y}) => {
+
+    // Absolute coordinates
     const coordX = Math.abs(this.axisXDefault - Math.abs(x))
     const coordY = Math.abs(this.axisYDefault - Math.abs(y))
 
     const isLeft = x < 134 ;
     const isTop = y < 259;
 
+    // Distance from the center
     const c = calculateC(coordY, coordX)
-
+    // Max distance limit
     const maxC = c > this.axisXDefault ? this.axisXDefault : c;
+
+    // Alfa angle
     const alfa = getDegrees(Math.atan(coordY/coordX))
-    // const beta = 90 - alfa
 
-    const powerDecrease = alfa/90
+    // Angle ratio
+    const powerDecrease = alfa / 90
 
+    // Max power depends on dinstance from center of display
     const maxPower = (maxC/this.axisXDefault) * 255
 
     const right = (isTop ? 1 : -1) *  (maxPower)
@@ -145,12 +134,10 @@ export default class AnimationExample extends Component {
     const leftCalc = left ? left : 0
     const rightCalc = right ? right : 0
 
-    const power = {
+    this.power = {
       right: parseInt((!isLeft ? leftCalc : rightCalc), 10),
       left: parseInt(( isLeft ? leftCalc : rightCalc), 10)
     }
-
-    this.power=power;
   }
 
   power = {
@@ -158,66 +145,41 @@ export default class AnimationExample extends Component {
     left: 0
   }
 
-
   interval = null;
 
   sendData = () => {
     this.interval = setInterval(()=>{
-
       if(this.state.connected){
         BluetoothSerial.write(`${this.power.left} ${this.power.right}\n`)
           .catch((err) => console.error(err.message))
-
-        console.log(this.power)
       }
-
     },200)
   }
 
-
-
-  connect = () => {
-    const { device }  = this.state;
-      console.log('try to connect', device, device.id)
-
-  }
-
   render() {
-    const { edgeLength, panAnimation, connected } = this.state;
+    const { edgeLength, panAnimation, connected, scaleAnimation } = this.state;
 
     const styles = {
       height: edgeLength,
       width: edgeLength,
-      backgroundColor: 'blue',
       transform: [
         { translateX: panAnimation.x },
         { translateY: panAnimation.y },
-        { scaleX: this.state.scaleAnimation },
-        { scaleY: this.state.scaleAnimation },
-      ]
+        { scaleX: scaleAnimation },
+        { scaleY: scaleAnimation },
+      ],
+      backgroundColor: connected ? 'blue' : 'red'
     };
 
-    styles.backgroundColor = connected ? '#F5FCFF' : 'red';
-
     return (
-      <View style={styles.container}>
+      <View>
         <Animated.View
           style={styles}
           {...this.panResponder.panHandlers}
         />
-
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
 
 AppRegistry.registerComponent('bbcar', () => AnimationExample);
