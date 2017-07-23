23eb355c00862abf738ac988f01d0afa4f1920c4
Antonin <antonin.hackenberg@gmail.com>
2017-07-23 02:22:55 +0200

    Refactor components

--- src/index.js
+++ src/index.js
@@ -13,72 +13,23 @@ import {
 
 import BluetoothSerial from 'react-native-bluetooth-serial'
 
+import AnimationWrapper from './AnimationWrapper'
 
-import { decreasePower, computePower, getRadians, getDegrees, calculateC, calculateAlfa } from './calculations'
-
-export default class AnimationExample extends Component {
+export default class BluetoothWrapper extends Component {
   constructor(props) {
     super(props);
-    const { width, height } = Dimensions.get('window');
-    const edgeLength = 50;
-
-    const axisX = (width / 2) - (edgeLength / 2);
-    const axisY = (height / 2) - (edgeLength / 2);
-
-    this.axisX = axisX;
-    this.axisY = axisY;
-
-    this.axisXDefault = axisX;
-    this.axisYDefault = axisY;
-
     this.state = {
       connected: false,
-      coordX: 0,
-      coordY: 0,
-      axisX,
-      axisY,
-      edgeLength,
-      panAnimation: new Animated.ValueXY({ x: axisX , y: axisY }),
-      scaleAnimation: new Animated.Value(1),
     }
   }
 
   componentWillMount() {
-    this.state.panAnimation.x.addListener((event) => {this.axisX = event.value; this.calculatePower({x: event.value, y: this.axisY})});
-    this.state.panAnimation.y.addListener((event) => {this.axisY = event.value; this.calculatePower({y: event.value, x: this.axisX})});
-
-    this.panResponder = PanResponder.create({
-      onMoveShouldSetResponderCapture: () => true,
-      onMoveShouldSetPanResponderCapture: () => true,
-      onPanResponderGrant: (e) => {
-        this.state.panAnimation.setOffset({ x: this.axisX, y: this.axisY });
-        this.state.panAnimation.setValue({ x: 0, y: 0 });
-        Animated.spring(this.state.scaleAnimation, {
-          toValue: 1.5,
-        }).start();
-        // Start to send data to Car
-        this.sendData()
-      },
-      onPanResponderMove: Animated.event([
-        null, { dx: this.state.panAnimation.x, dy: this.state.panAnimation.y },
-      ]),
-      onPanResponderRelease: () => {
-        Animated.spring(this.state.scaleAnimation, {
-          toValue: 1,
-        }).start();
-
-        // Clear coordinates
-        this.state.panAnimation.setValue({ x: 0, y: 0 });
-        this.interval && clearInterval(this.interval)
-      }
-    });
-
     this.findConnection()
 
   };
 
   findConnection = () => {
-    console.log('try to connect')
+    console.log('try to connect');
     Promise.all([
       BluetoothSerial.isEnabled(),
       BluetoothSerial.list()
@@ -99,65 +50,30 @@ export default class AnimationExample extends Component {
     BluetoothSerial.connect(device.id)
       .then((res) => {
         console.log(`Connected to device`, res)
-        this.setState({connected: true })
-
+        this.setState({ connected: true })
       })
       .catch(err => {
         alert('Amigo, no funciona!!')
-        this.setState({connected: false })
+        this.setState({ connected: false })
       })
   }
 
   disconnect = () => {
-    BluetoothSerial.disconnect() && this.setState({connected: false})
+    BluetoothSerial.disconnect() && this.setState({ connected: false })
   }
 
   handleConnectButton = () => {
     const { connected, device } = this.state
 
     connected ? this.disconnect(device.id) : this.findConnection()
-
-  }
+  };
 
   componentWillUnmount() {
-    this.state.panAnimation.x.removeAllListeners();
-    this.state.panAnimation.y.removeAllListeners();
     clearInterval(this.interval)
   };
 
-  calculatePower = ({x, y}) => {
-
-    // Absolute coordinates
-    const coordX = Math.abs(this.axisXDefault - Math.abs(x))
-    const coordY = Math.abs(this.axisYDefault - Math.abs(y))
-
-    const isLeft = x < 134 ;
-    const isTop = y < 259;
-
-    // Distance from the center
-    const c = calculateC(coordY, coordX)
-    // Max distance limit
-    const maxC = c > this.axisXDefault ? this.axisXDefault : c;
-
-    // Alfa angle
-    const alfa = getDegrees(Math.atan(coordY/coordX))
-
-    // Angle ratio
-    const powerDecrease = alfa / 90
-
-    // Max power depends on dinstance from center of display
-    const maxPower = (maxC/this.axisXDefault) * 255
-
-    const right = (isTop ? 1 : -1) *  (maxPower)
-    const left = (isTop ? 1 : -1) * ( powerDecrease * maxPower)
-
-    const leftCalc = left ? left : 0
-    const rightCalc = right ? right : 0
-
-    this.power = {
-      right: parseInt((!isLeft ? leftCalc : rightCalc), 10),
-      left: parseInt(( isLeft ? leftCalc : rightCalc), 10)
-    }
+  setPower = (power) => {
+    this.power = power
   }
 
   power = {
@@ -168,40 +84,31 @@ export default class AnimationExample extends Component {
   interval = null;
 
   sendData = () => {
-    this.interval = setInterval(()=>{
-      if(this.state.connected){
+    this.interval = setInterval(() => {
+      const connected = BluetoothSerial.isConnected()
+      if(connected){
         BluetoothSerial.write(`${this.power.left} ${this.power.right}\n`)
           .catch((err) => console.error(err.message))
+      }else{
+        this.setState({connected})
       }
     },100)
   }
 
+  clearInterval = () => {
+    this.interval && clearInterval(this.interval)
+  }
 
   render() {
-    const { edgeLength, panAnimation, connected, scaleAnimation } = this.state;
-
-    const styles = {
-      height: edgeLength,
-      width: edgeLength,
-      transform: [
-        { translateX: panAnimation.x },
-        { translateY: panAnimation.y },
-        { scaleX: scaleAnimation },
-        { scaleY: scaleAnimation },
-      ],
-      backgroundColor: connected ? 'blue' : 'red'
-    };
+    const { connected } = this.state;
 
     return (
       <View>
-        <Button title={connected ? 'Disconnect' : 'Connect'} onPress={this.handleConnectButton}/>
-        <Animated.View
-          style={styles}
-          {...this.panResponder.panHandlers}
-        />
+        <Button title={connected ? 'Disconnect' : 'Connect'} onPress={this.handleConnectButton} />
+        <AnimationWrapper connected={connected} sendData={this.sendData} setPower={this.setPower} clear={this.clearInterval}  />
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent('bbcar', () => AnimationExample);
+AppRegistry.registerComponent('bbcar', () => BluetoothWrapper);
